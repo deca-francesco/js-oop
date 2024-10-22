a46c8de4f7eb2808d580deb1fe821bb64c045b15
deca-francesco <francescodecarlo70@gmail.com>
2024-10-22 16:16:02 +0200

    UPDATE: method informazioni()

--- main.js
+++ main.js
@@ -16,14 +16,24 @@ class Veicolo {
     }
 
     informazioni() {
-        return this;
+        // return this
+        const info = [];
+        for (const key in this) {
+            let element = this[key];
+            info.push(element);
+        }
+        return info.join(" | ")
     }
 
     calcolaEta() {
         const now = new Date().getFullYear();
         // console.log(now);
-        return  `il veicolo ha ${now - this.anno} anni`;
+        return `il veicolo ha ${now - this.anno} anni`;
     }
+}
+
+class Automobile extends Veicolo {
+
 }
 
 const panda = new Veicolo("Fiat", 2019, "blu", 5, "benzina");

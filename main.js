fbf6420295ac1b30508bee646c2c156a7fdca91a
deca-francesco <francescodecarlo70@gmail.com>
2024-10-22 18:30:23 +0200

    ADD: bonus array

--- main.js
+++ main.js
@@ -48,3 +48,31 @@ const corsa = new Automobile("Opel", 2020, "nera", 4, "benzina");
 console.log(corsa.porteEcarburante());
 
 
+
+const autoItaliane = [
+    fiorino = new Automobile("Fiat", 1997, "bianco", 3, "metano"),
+    giulia = new Automobile("Alfa Romeo", 2021, "blu", 5, "diesel"),
+    fiat500 = new Automobile("Fiat", 2020, "bianco", 5, "elettrica"),
+    doblo = new Automobile("Fiat", 2005, "blu", 5, "diesel"),
+];
+
+
+const carburanti = [];
+autoItaliane.forEach(auto => carburanti.push(auto.carburante))
+console.log(carburanti);
+
+
+const colori = [];
+for (let i = 0; i < autoItaliane.length; i++) {
+    const auto = autoItaliane[i];
+    colori.push(auto.colore);
+}
+console.log(colori);
+
+
+const nomi = autoItaliane.map(auto => auto.marca);
+console.log(nomi);
+
+const elettricheArray = autoItaliane.filter(auto => auto.carburante === "elettrica");
+const elettricheMarca = elettricheArray.map(auto => auto.marca);
+console.log(elettricheMarca);
